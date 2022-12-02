c91027f9b021efade545994d6ea4d0b8bdfedacd
liufangf <liufangf@bu.edu>
2022-12-02 17:17:19 -0500

    javascript change

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 // put your globals here - hint: select elements from the HTML
+const countBtn = document.getElementById('countBtn');
+const textInput = document.getElementById('text-area');
+const getWordBtn = document.getElementById('findBtn');
+const wordIput = document.getElementById('wordFoundInput');
+const findWord = document.getElementById('findWordInput');
+const wordFound = document.getElementById('wordFoundInput');
+const wordCount = document.getElementById('wordCountInput');
 
-
+countBtn.addEventListener("click", countWords);
+findBtn.addEventListener("click", findWords);
 // dont' forget to add event listeners to teh buttons
 
 function countWords() {
-  // your code here
-  // call printData
+  let words = textInput.value.split(' ');
+  let total = 0;
+  for(i=0; i<words.length; i++){
+    total +=1;
+  }
+  printData(total,wordCount);
 }
 
 function findWords() {
-  // put your local variable for the empty array here
-  // remainder of your code follows
-  // call printData
+  let foundwords=0;
+  textInput.value.split(' ').forEach(function(i){
+      if(i===findWord.value){
+        foundwords +=1;
+      }
+  });
+  printData(foundwords,wordFound);
 }
 
 // change param1 and param2 to identifiers that make sense
-function printData(param1, param2) {
-  // your code here... one line!
+function printData(data, dom) {
+    dom.value = data;
+}
+function printData(data,dom) {
+  dom.value = data;
 }
